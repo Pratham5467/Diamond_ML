4d9da16632f1b0f2a4f76b2ed9bf42c16f4fe96c
Pratham Srivastava <srivastavapratham52@gmail.com>
2024-08-08 13:59:44 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages,setup
+from typing import List
+
+hypen_dot_e = '-e .'
+
+def fun(file_path):
+    req = []
+    with open(file_path) as obj:
+        req = obj.readlines()
+        req = [x.replace('\n','') for x in req]
+        if hypen_dot_e in req:
+            req.remove(hypen_dot_e)
+        return req
+
+setup(
+    name='DiamondPrice',
+    version='0.0.1',
+    author='Pratham Srivastava',
+    author_email='srivastavapratham52@gmail.com',
+    install_requires=fun('requirements.txt'),
+    packages=find_packages()
+)
